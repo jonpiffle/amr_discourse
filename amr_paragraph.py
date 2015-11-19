b8277796c706390434677463046f18786ee4563a
Jon Pfeil <piffle21@gmail.com>
2015-11-18 22:34:03 -0500

    Added AMRParagraph and a SlidingWindowGenerator that generates paragraphs

---
+++ amr_paragraph.py
@@ -0,0 +1,48 @@
+import itertools
+
+from file_parser import FileParser
+
+def window(iterable, size):
+    """
+    Iterates over an iterable size elements at a time
+    [1, 2, 3, 4, 5], 3 ->
+        [1, 2, 3],
+        [2, 3, 4],
+        [3, 4, 5]
+    """
+
+    iters = itertools.tee(iterable, size)
+    for i in range(1, size):
+        for each in iters[i:]:
+            next(each, None)
+    return zip(*iters)
+
+
+class SlidingWindowGenerator(object):
+    def __init__(self, file_entries):
+        self.file_entries = file_entries
+
+    def generate(self, k=5):
+        amr_paragraphs = []
+
+        amr_documents = itertools.groupby(self.file_entries, lambda x: x.document_name)
+        for document_name, file_entries in amr_documents:
+            for subset in window(file_entries, k):
+                amr_paragraphs.append(AMRParagraph(document_name, subset))
+        return amr_paragraphs
+
+
+class AMRParagraph(object):
+    def __init__(self, document_name, amr_sentences=None):
+        if amr_sentences is None:
+            amr_sentences = []
+        self.amr_sentences = amr_sentences
+        self.document_name = document_name
+
+
+if __name__ == '__main__':
+    entries = FileParser().parse('amr.txt')
+    swg = SlidingWindowGenerator(entries)
+    paragraphs = swg.generate(k=5)
+    print(len(paragraphs))
+    print([s.entry_id for s in paragraphs[0].amr_sentences])
