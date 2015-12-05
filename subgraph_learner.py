224c8bcba61c5b086e6c487c319a1e06491b49d8
Jon Pfeil <piffle21@gmail.com>
2015-12-04 21:33:47 -0500

    added initial training/test system with 2 features

--- subgraph_learner.py
+++ subgraph_learner.py
@@ -1,6 +1,7 @@
 from file_parser import FileParser
 from amr_paragraph import SlidingWindowGenerator
 from partition import Partition
+from sklearn.linear_model import LogisticRegression
 import random
 
 def union_all(list_of_sets):
@@ -9,8 +10,8 @@ def union_all(list_of_sets):
         s = s | set(next_s)
     return s
 
-def generate_paragraphs(filename):
-    entries = FileParser().parse(filename, limit=100)
+def generate_paragraphs(filename, limit=None):
+    entries = FileParser().parse(filename, limit)
     swg = SlidingWindowGenerator(entries)
     paragraphs = swg.generate(k=2)
     return paragraphs
@@ -43,10 +44,11 @@ def generate_features(partition):
 
 def generate_instances_and_labels(paragraphs):
     instances, labels = [], []
-    for paragraph in paragraphs:
+    for i, paragraph in enumerate(paragraphs):
         try:
             positive_instance = get_positive_instance(paragraph)
-        except ValueError:
+        except (ValueError, RuntimeError) as e:
+            print(i, e)
             continue
         instances.append(generate_features(positive_instance))
         labels.append(1)
@@ -56,8 +58,12 @@ def generate_instances_and_labels(paragraphs):
         labels += [0] * len(negative_instances)
     return instances, labels
 
-train = generate_paragraphs('amr.txt')
-#test = generate_paragraphs('amr_test.txt')
+
+train = generate_paragraphs('amr.txt', limit=100)
+test = generate_paragraphs('amr_test.txt', limit=10)
 instances, labels = generate_instances_and_labels(train)
-print(instances)
-print(labels)
+reg = LogisticRegression(class_weight='auto')
+reg.fit(instances, labels)
+test_instances, test_labels = generate_instances_and_labels(test)
+for proba, label in zip(reg.predict_proba(test_instances), test_labels):
+    print(proba, label)
