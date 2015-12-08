240810ab82299fedb0f0a4e43b10a4b2ba91e91a
Jon Pfeil <piffle21@gmail.com>
2015-12-08 13:28:42 -0500

    change scorers to have separate train and load methods

--- scorer.py
+++ scorer.py
@@ -1,16 +1,28 @@
 import os, pickle, sys
 
+import numpy as np
+
 from sklearn.linear_model import LogisticRegression, Ridge
 from sklearn.metrics import roc_auc_score, classification_report
+
+def summary(lst):
+    # set features to 0 if empty lst (like cases where list is too small to get 2-step similarity)
+    if len(lst) == 0:
+        return [0, 0, 0, 0]
+
+    return [np.mean(lst), np.std(lst), min(lst), max(lst)]
 
 class Scorer(object):
     def __init__(self):
         pass
 
-    def train(self, train_instances, train_labels, use_cache=True):
+    def train(self, train_instances, train_labels, update_cache=True):
         pass
 
     def test(self, test_instances, test_labels):
+        pass
+
+    def load(self):
         pass
 
     def evaluate(self, test_instance):
@@ -20,14 +32,18 @@ class Scorer(object):
 class SubgraphSelectionScorer(Scorer):
     def __init__(self):
         self.classifier = LogisticRegression(class_weight='auto')
+        self.cache_filename = 'subgraph_selection_scorer_reg.pickle'
 
-    def train(self, train_instances, train_labels, use_cache=True):
-        train_filename = 'subgraph_selection_scorer_reg.pickle'
-        if use_cache and os.path.exists(train_filename):
-            self.classifier = pickle.load(open(train_filename, 'rb'))
+    def train(self, train_instances, train_labels, update_cache=True):
+        self.classifier.fit(train_instances, train_labels)
+        if update_cache:
+            pickle.dump(self.classifier, open(self.cache_filename, 'wb'))
+
+    def load(self):
+        if os.path.exists(self.cache_filename):
+            self.classifier = pickle.load(open(self.cache_filename, 'rb'))
         else:
-            self.classifier.fit(train_instances, train_labels)
-            pickle.dump(self.classifier, open(train_filename, 'wb'))
+            raise Exception("No classifier exists! Must call train with update_cache=True")
 
     def test(self, test_instances, test_labels):
         predictions = self.classifier.predict(test_instances)
@@ -43,25 +59,28 @@ class OrderScorer(Scorer):
 
     def __init__(self):
         self.classifier = Ridge(alpha=0.1)
+        self.cache_filename = 'subgraph_order_scorer_reg.pickle'
 
-    def train(self, train_instances, train_labels, use_cache=True,
+    def train(self, train_instances, train_labels, update_cache=True,
               sample_weight=None):
         """
         Trains a scorer to score the quality of an ordering of sentences
         Loads from cache if available
         """
-        cache = 'subgraph_order_scorer_reg.pickle'
-        if use_cache and os.path.exists(cache):
-            self.classifier = pickle.load(open(cache, 'rb'))
-        else:
-            self.classifier.fit(train_instances, train_labels,
-                                sample_weight=sample_weight)
-            pickle.dump(self.classifier, open(cache, 'wb'))
+        self.classifier.fit(train_instances, train_labels, sample_weight=sample_weight)
+        if update_cache:
+            pickle.dump(self.classifier, open(self.cache_filename, 'wb'))
 
     def test(self, test_instances, test_labels):
         """ Uses test set to evaluate the performance of the scorer and print it out """
         scores = self.classifier.predict(test_instances)
         # TODO: print report
+
+    def load(self):
+        if os.path.exists(self.cache_filename):
+            self.classifier = pickle.load(open(self.cache_filename, 'rb'))
+        else:
+            raise Exception("No classifier exists! Must call train with update_cache=True") 
 
     def evaluate(self, test_instance):
         """ Applies the scoring function to a given test instance """
@@ -69,16 +88,37 @@ class OrderScorer(Scorer):
 
 class PipelineScorer(Scorer):
     def __init__(self):
-        pass
+        self.subgraph_optimizer = None
+        self.order_optimizer = None
+        self.cache_filename = 'pipeline_scorer_reg.pickle'
 
-    def train(self, train_instances, train_labels, use_cache=True):
-        pass
+    def train(self, subgraph_optimizer, order_optimizer, update_cache=True):
+        self.subgraph_optimizer = subgraph_optimizer
+        self.order_optimizer = order_optimizer
+        if update_cache:
+            pickle.dump((self.subgraph_optimizer, self.order_optimizer), open(self.cache_filename, 'wb'))
 
-    def test(self, test_instances, test_labels):
-        pass
+    def test(self, test_paragraphs, subgraph_strategy='greedy', order_strategy='greedy'):
+        print('subgraph_strategy: %s' % subgraph_strategy)
+        print('order_strategy: %s' % order_strategy)
+        print('mean, std_dev, min, max kendall tau: ', 
+              summary([self.evaluate(test_paragraph, subgraph_strategy, order_strategy) for test_paragraph in test_paragraphs]))
+        print()
 
-    def evaluate(self, test_instance):
-        pass
+    def load(self):
+        if os.path.exists(self.cache_filename):
+            self.subgraph_optimizer, self.order_optimizer = pickle.load(open(self.cache_filename, 'rb'))
+        else:
+            raise Exception("No classifier exists! Must call train with update_cache=True") 
+
+    def evaluate(self, test_paragraph, subgraph_strategy='greedy', order_strategy='greedy'):
+        best_graph_partition = self.subgraph_optimizer.optimize(test_paragraph, subgraph_strategy)
+        best_graph_partition_with_order = self.order_optimizer.optimize(best_graph_partition, order_strategy)
+        target_root_ordering = [frozenset(s.get_roots()) for s in test_paragraph.sentence_graphs()]
+        test_root_ordering = best_graph_partition_with_order.get_ordered_root_sets()
+        root_swaps = get_root_swaps(test_root_ordering, target_root_ordering)
+        print(subgraph_strategy, order_strategy, root_swaps)
+        return root_swaps
 
 def get_root_swaps(root_partitions, goal_root_partitions):
     def get_goal_partition_index(root):
@@ -93,3 +133,33 @@ def get_root_swaps(root_partitions, goal_root_partitions):
                         count += 1
     return count
 
+if __name__ == '__main__':
+    from subgraph_learner import generate_train_test, generate_paragraphs
+    from order_learner import get_features, add_negative_examples
+    from optimizer import SubgraphOptimizer, OrderOptimizer
+
+    train_instances, train_labels, test_instances, test_labels, test = generate_train_test(use_cache=False)
+    subgraph_scorer = SubgraphSelectionScorer()
+    subgraph_scorer.load()
+    subgraph_optimizer = SubgraphOptimizer(subgraph_scorer)
+
+    #train = generate_paragraphs('amr.txt', limit=500, k=5)
+    #examples, labels = add_negative_examples(train, 20)
+    #n = len(examples)
+    #weights = n - np.bincount(labels)
+    #features = np.array([get_features(e, e.sentence_graphs()) for e in examples])
+    order_scorer = OrderScorer()
+    order_scorer.load()
+    #order_scorer.train(features, labels, sample_weight=[weights[i] for i in labels])
+    order_optimizer = OrderOptimizer(order_scorer)
+
+    pipeline_scorer = PipelineScorer()
+    pipeline_scorer.train(subgraph_optimizer, order_optimizer)
+    test = test[:30]
+
+    pipeline_scorer.test(test, subgraph_strategy='baseline', order_strategy='baseline')
+    #pipeline_scorer.test(cleaned_test, subgraph_strategy='greedy', order_strategy='greedy')
+    #pipeline_scorer.test(cleaned_test, subgraph_strategy='greedy', order_strategy='anneal')
+    #print(pipeline_scorer.evaluate(t, subgraph_strategy='baseline', order_strategy='baseline'))
+    #print(pipeline_scorer.evaluate(t, subgraph_strategy='greedy', order_strategy='greedy'))
+    #print(pipeline_scorer.evaluate(t, subgraph_strategy='greedy', order_strategy='anneal'))
